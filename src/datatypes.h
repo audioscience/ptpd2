589814fffffd536efa621803e061a2d1012c763d
eharris <eharris@f0e33cc9-18b5-42c2-a215-ec7f8ade0af0>
2011-02-20 12:13:46 +0000

    Add support for DNS lookups of the grandmaster when using unicast.
    git-svn-id: https://ptpd2.svn.sourceforge.net/svnroot/ptpd2@13 f0e33cc9-18b5-42c2-a215-ec7f8ade0af0

--- src/datatypes.h
+++ src/datatypes.h
@@ -2,6 +2,7 @@
 #define DATATYPES_H_
 
 #include <stdio.h> 
+#include <sys/param.h>
 
 /*Struct defined in spec*/
 
@@ -415,7 +416,7 @@ typedef struct {
 	Boolean	displayStats;
 	Boolean	csvStats;
 	Boolean displayPackets;
-	Octet unicastAddress[NET_ADDRESS_LENGTH];
+	Octet unicastAddress[MAXHOSTNAMELEN];
 	Integer16 ap, ai;
 	Integer16 s;
 	TimeInternal inboundLatency, outboundLatency;
