bef4c403c4c3256e19e444ab9df9a14f04ff2f25
Eliot Blennerhassett <eblennerhassett@audioscience.com>
2012-03-28 16:24:17 +1300

    Script with cross compile options

---
+++ src/make-eliots.sh
@@ -0,0 +1,10 @@
+
+# Location of target linux headers
+ARM_KBUILD_OUTPUT=$HOME/asi/external/linux-omap3/usr
+# Root of NFS filesystem for network boot
+ARM_NFS_ROOT=$HOME/asi/firmware/ezsdk_target_fs_5_03_01_15
+# ARM c compiler
+ARM_CC=arm-none-linux-gnueabi-gcc
+
+make KBUILD_OUTPUT=$ARM_KBUILD_OUTPUT CC=$ARM_CC
+cp ptpd2 $ARM_NFS_ROOT/home/root/
